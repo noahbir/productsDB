3034548e270266c169f7b6388d31323364088e2e
noahbir <noah.birmanns@gmail.com>
2025-05-12 15:42:35 +0200

    adding details page for each product

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ app.set('view engine', 'ejs');
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
-    res.render('index', { products });
+    res.render('products/index', { products });
+})
+
+app.get('/products/:id', async (req, res) => {
+	const { id } = req.params;
+	const product = await Product.findById(id);
+	res.render('products/show', { product })
 })
 
 app.listen(3000, () => {
