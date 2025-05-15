f33e4bebdbb95d28ea51b6974bed3a699fdc1f87
noahbir <noah.birmanns@gmail.com>
2025-05-15 13:04:15 +0200

    deleting and categories

--- index.js
+++ index.js
@@ -21,13 +21,21 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true} ));
 app.use(methodOverride('_method'));
 
+const categories = ['fruit', 'vegetable', 'dairy'];
+
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index', { products });
+	const { category }  = req.query;
+	if(category) {
+		const products = await Product.find({category: category});
+		res.render('products/index', { products, category });
+	} else {
+		const products = await Product.find({});
+		res.render('products/index', { products, category: 'All' });
+	}
 })
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new');
+    res.render('products/new', { categories });
 })
 
 app.post('/products', async (req, res) => {
@@ -45,13 +53,19 @@ app.get('/products/:id', async (req, res) => {
 app.get('/products/:id/edit', async (req, res) => {
 	const { id } = req.params;
 	const product = await Product.findById(id);
-    res.render('products/edit', { product });
+    res.render('products/edit', { product, categories });
 })
 
 app.put('/products/:id', async (req, res) => {
 	const { id } = req.params;
 	const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
 	res.redirect(`/products/${ product._id }`);
+})
+
+app.delete('/products/:id', async (req, res) => {
+	const { id } = req.params;
+	const deletedProduct = await Product.findByIdAndDelete(id);
+	res.redirect('/products');
 })
 
 app.listen(3000, () => {
